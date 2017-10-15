639725e689b8c3ac9833fd24fc44cd291c6b9ac5
zengtianshengz <zengtianshengz@163.com>
2017-10-15 22:00:38 +0800

    ng

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,83 @@ const app = getApp();
 
 Page({
   data: {
-    motto: 'Hello World',
-    toView: 'red',
-    scrollTop: 100
+    obj_listItem: {
+      nickName: '曾田生x',
+      avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+      createTime: '今天 09：06',
+      address: '福鼎家园',
+      description: '说的就案件的就是就看看模块时代是的搜东山口卡就是卡死进度款几十块几点开始',
+      imgList: [
+        '../../images/matter/img1.jpeg',
+        '../../images/matter/img6.jpeg',
+        '../../images/matter/img7.jpeg',
+        '../../images/matter/img8.jpeg',
+        '../../images/matter/img9.jpeg'
+      ]
+    },
+    arr_listItems: [
+      {
+        nickName: '曾田生x1',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+        createTime: '今天 09：06',
+        address: '福鼎家园',
+        description: '是看到就是看到司机将是看到几十块达康书记看就看',
+        imgList: [
+          '../../images/matter/img9.jpeg'
+        ]
+      },
+      {
+        nickName: '曾田生x2',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+        createTime: '今天 09：06',
+        address: '福鼎家园',
+        description: '说的就案件的就是就看看模块时代是的搜东山口卡就是卡死进度款几十块几点开始',
+        imgList: []
+      },
+      {
+        nickName: '曾田生x3',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+        createTime: '今天 09：06',
+        address: '福鼎家园',
+        description: '说的就是开机定',
+        imgList: [
+          '../../images/matter/img7.jpeg',
+          '../../images/matter/img8.jpeg'
+        ]
+      },
+      {
+        nickName: '曾田生x4',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+        createTime: '今天 09：06',
+        address: '福鼎家园',
+        description: '说的就是看到世界第四季我进件开卷考试',
+        imgList: [
+          '../../images/matter/img1.jpeg',
+          '../../images/matter/img2.jpeg',
+          '../../images/matter/img3.jpeg',
+          '../../images/matter/img4.jpeg',
+          '../../images/matter/img5.jpeg'
+        ]
+      },
+      {
+        nickName: '曾田生x5',
+        avatarUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKApG4aicW5835FUgOBY75jRDaYt8B84YIQL6v9J9lI4KEPA1nicGNxtdlI5iamIosXiaRqiavDAXmW2rQ/0',
+        createTime: '今天 09：06',
+        address: '福鼎家园',
+        description: '速度快扩扩扩',
+        imgList: [
+          '../../images/matter/img1.jpeg',
+          '../../images/matter/img2.jpeg',
+          '../../images/matter/img3.jpeg',
+          '../../images/matter/img4.jpeg',
+          '../../images/matter/img5.jpeg',
+          '../../images/matter/img6.jpeg',
+          '../../images/matter/img7.jpeg',
+          '../../images/matter/img8.jpeg',
+          '../../images/matter/img9.jpeg'
+        ]
+      }
+    ]
   },
   onLoad() {
 
