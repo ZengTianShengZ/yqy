82b42e67e9c4c99fb6915d8740a3bac86a972169
zengtianshengz <zengtianshengz@163.com>
2017-11-08 22:29:35 +0800

    nt

--- pages/sendDynamic/senddynamic.js
+++ pages/sendDynamic/senddynamic.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const storage = require('../../libs/lib/storage')
+const qiniuUploader = require("../../utils/qiniuUploader");
 
 const app = getApp();
 
@@ -70,6 +71,25 @@ Page({
         _this.setData({
            str_address: res.name
         })
+      }
+    })
+  },
+  ttUploadFileBtn(){
+    wx.chooseImage({
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        // 交给七牛上传
+        qiniuUploader.upload(tempFilePaths[0], (res) => {
+          console.logres()
+        }, (error) => {
+          console.log('error: ' + error);
+        }, {
+            region: 'ECN',
+            domain: 'oyn5he3v2.bkt.clouddn.com', // // bucket 域名，下载资源时用到。如果设置，会在 success callback 的 res 参数加上可以直接使用的 ImageURL 字段。否则需要自己拼接
+            key: 'xxxxxx.jpg', // [非必须]自定义文件 key。如果不设置，默认为使用微信小程序 API 的临时文件名
+            // 以下方法三选一即可，优先级为：uptoken > uptokenURL > uptokenFunc
+            uptokenURL: 'https://yqy.mynatapp.cc/v2/qnUptoken', // 从指定 url 通过 HTTP GET 获取 uptoken，返回的格式必须是 json 且包含 uptoken 字段，例如： {"uptoken": "[yourTokenString]"}
+          });
       }
     })
   }
